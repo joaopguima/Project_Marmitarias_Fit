82a6fa3ee8ff9e3f37e2c2d233b21dd2e1348fd3
Joao Pedro <joaopedro.jusus@gmail.com>
2024-06-20 15:48:39 -0300

    Ajustando controller

--- src/Controller/ProdutoController.java
+++ src/Controller/ProdutoController.java
@@ -1,5 +1,38 @@
 package Controller;
 
-public class ProdutoController {
+import Entidades.Produto;
+import Repository.ProdutoRepository;
+
+public class ProdutoController implements ProdutoRepository {
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cadastrar(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
